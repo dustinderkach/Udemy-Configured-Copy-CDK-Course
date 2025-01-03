0c5a6acb23e48c7ef1edcb4234c67c94a49d126a
DustinDerkach <dustinderkach2@gmail.com>
2025-01-02 21:41:17 -0600

    changed slack web hook url

--- space-finder/src/services/monitor/handler.ts
+++ space-finder/src/services/monitor/handler.ts
@@ -1,18 +1,18 @@
 import { SNSEvent } from "aws-lambda";
 
-const webHookUrl = 'https://hooks.slack.com/services/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';
+const webHookUrl =
+	"https://hooks.slack.com/services/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";
+//old hook: 'https://hooks.slack.com/services/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'
 
 async function handler(event: SNSEvent, context) {
-    for (const record of event.Records) {
-
-        await fetch(webHookUrl, {
-            method: 'POST',
-            body: JSON.stringify({
-                "text": `Huston, we have a problem: ${record.Sns.Message}`
-            })
-        })
-    }
+	for (const record of event.Records) {
+		await fetch(webHookUrl, {
+			method: "POST",
+			body: JSON.stringify({
+				text: `Huston, we have a problem: ${record.Sns.Message}`,
+			}),
+		});
+	}
 }
 
-
-export { handler }
+export { handler };
