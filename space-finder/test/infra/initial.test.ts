0823afb15131280f7a88eb07a5a0a06a76d96a25
DustinDerkach <dustinderkach2@gmail.com>
2025-01-05 16:21:14 -0600

    did 95

--- space-finder/test/infra/initial.test.ts
+++ space-finder/test/infra/initial.test.ts
@@ -1,11 +1,30 @@
+import { App } from "aws-cdk-lib";
+import { MonitorStack } from "../../src/infra/stacks/MonitorStack";
+import { Template } from "aws-cdk-lib/assertions";
 
+describe("Initial test suite", () => {
+	let monitorStackTemplate: Template;
 
+	beforeAll(() => {
+		const testApp = new App({
+			outdir: "cdk.out",
+		});
 
-describe('Initial test suite', () => {
-    test('initial test', () => {
-        // Stps: arrange, act, assert
+		const monitorStack = new MonitorStack(testApp, "MonitorStack");
+		monitorStackTemplate = Template.fromStack(monitorStack);
+	});
 
-        expect(true).toBeTruthy();
+	test("initial test", () => {
+		monitorStackTemplate.hasResourceProperties("AWS::Lambda::Function", {
+			Handler: "index.handler",
+			Runtime: "nodejs18.x",
+		});
+	});
 
-    })
-})
+	test("Sns topic properties", () => {
+		monitorStackTemplate.hasResourceProperties("AWS::SNS::Topic", {
+			DisplayName: "AlarmTopic",
+			TopicName: "AlarmTopic",
+		});
+	});
+});
