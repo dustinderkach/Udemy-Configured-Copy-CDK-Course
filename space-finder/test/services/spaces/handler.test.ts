c1154ffec2ffacd756fab53af3e3c65101b37bdd
DustinDerkach <dustinderkach2@gmail.com>
2025-01-09 20:03:44 -0600

    on 104

--- space-finder/test/services/spaces/handler.test.ts
+++ space-finder/test/services/spaces/handler.test.ts
@@ -3,55 +3,57 @@ import { handler } from "../../../src/services/spaces/handler";
 
 ///This may not be a good way to mock the DynamoDBClient, it maybe better to use the AWS SDK mock
 
-jest.mock('@aws-sdk/client-dynamodb', () => {
-    return {
-        DynamoDBClient: jest.fn().mockImplementation(() => {
-            return {
-                send: jest.fn().mockImplementation(() => {
-                    return {
-                        Items: someItems
-                    }
-                })
-            }
-        }),
-        ScanCommand: jest.fn()
-    }
+jest.mock("@aws-sdk/client-dynamodb", () => {
+	return {
+		DynamoDBClient: jest.fn().mockImplementation(() => {
+			return {
+				send: jest.fn().mockImplementation(() => {
+					return {
+						Items: someItems,
+					};
+				}),
+			};
+		}),
+		ScanCommand: jest.fn(),
+	};
 });
 
-const someItems = [{
-    id: {
-        S: '123'
-    },
-    location: {
-        S: 'Paris'
-    }
-}]
+const someItems = [
+	{
+		id: {
+			S: "123",
+		},
+		location: {
+			S: "Paris",
+		},
+	},
+];
 
+describe("Spaces handler test suite", () => {
+	test("Returns spaces from dynamoDb", async () => {
+		const result = await handler(
+			{
+				httpMethod: "GET",
+			} as any,
+			{} as any
+		);
 
+		expect(result.statusCode).toBe(201);
+		const expectedResult = [
+			{
+				id: "123",
+				location: "Paris",
+			},
+		];
+		const parsedResultBody = JSON.parse(result.body);
+		expect(parsedResultBody).toEqual(expectedResult);
 
+		expect(DynamoDBClient).toHaveBeenCalledTimes(1);
+		expect(ScanCommand).toHaveBeenCalledTimes(1);
+	});
 
-describe('Spaces handler test suite', () => {
-
-    test('Returns spaces from dynamoDb', async () => {
-        const result = await handler({
-            httpMethod: 'GET'
-        } as any, {} as any);
-
-        expect(result.statusCode).toBe(201);
-        const expectedResult = [{
-            id: '123',
-            location: 'Paris'
-        }];
-        const parsedResultBody = JSON.parse(result.body);
-        expect(parsedResultBody).toEqual(expectedResult);
-        
-        expect(DynamoDBClient).toHaveBeenCalledTimes(1);
-        expect(ScanCommand).toHaveBeenCalledTimes(1);
-    });
-
-    afterAll(() => {     
-        jest.clearAllMocks();
-        jest.restoreAllMocks();   
-   });
-
-})
+	afterAll(() => {
+		jest.clearAllMocks();
+		jest.restoreAllMocks();
+	});
+});
